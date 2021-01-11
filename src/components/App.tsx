f46ccc978ca06a101063969d7188dfd165ac3b87
justin0979 <justinm0979@gmail.com>
2021-01-11 17:58:06 -0600

    add basic SASS setup

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -4,9 +4,6 @@ const App = (): JSX.Element => {
   return (
     <div className="app" data-test="appComponent">
       <div className="item i1">1</div>
-      <div className="item i3">3</div>
-      <div className="item i4">4</div>
-      <div className="item i5">5</div>
     </div>
   );
 };
